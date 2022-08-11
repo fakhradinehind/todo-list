fb136c677f1fe529c75ba0f25e409893af66e1fc
fakhradine hind <fakhradinehind@gmail.com>
2022-08-11 11:23:02 +0200

    template

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -1,30 +1,45 @@
 <template>
-  
+  <div class="align-items-center">
+      <h1>Project Todo-List</h1>
+      <br>
+          <div class="input-group">
+                <input type="text" class="form-control" placeholder="ajouter votre ..." >
+                <button class="btn btn-outline-secondary" type="button" >Ajouter</button>
+                <button class="btn btn-outline-secondary" type="button">Annuler</button>
+          </div>
+          <br>
+      <button class="btn btn-primary">Afficher</button>
+      <br>
+      <div>
+        <table class="table ">
+             <th >
+                  <td>Numero</td>
+                  <td>Todo item</td>
+                  <td>Action</td>
+             </th>
+             <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+             </tr>
+        </table>
+      </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  name: 'todo',
+ 
+  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.input-group{
+  padding-left: 200px;
+  padding-right: 200px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
