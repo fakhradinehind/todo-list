c410aaa9bc523a634538182b2105e8e3c36660b5
fakhradine hind <fakhradinehind@gmail.com>
2022-08-11 15:12:06 +0200

    action afficher bien fait

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -4,33 +4,62 @@
       <br>
           <div class="input-group">
                 <input type="text" class="form-control" placeholder="ajouter votre ..." >
-                <button class="btn btn-outline-secondary" type="button" >Ajouter</button>
+                <button class="btn btn-outline-secondary" type="button" @click="ajouter" >Ajouter</button>
                 <button class="btn btn-outline-secondary" type="button">Annuler</button>
           </div>
           <br>
-      <button class="btn btn-primary">Afficher</button>
+      <button class="btn btn-primary" @click="afficher">Afficher</button>
       <br>
-      <div>
-        <table class="table ">
-             <th >
-                  <td>Numero</td>
-                  <td>Todo item</td>
-                  <td>Action</td>
-             </th>
-             <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-             </tr>
-        </table>
-      </div>
+      <br>
+     <table class="table">
+  <thead>
+    <tr>
+      
+      <th scope="col">Numero</th>
+      <th scope="col">Todo</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="post in posts">
+      
+      <td>{{post.numero}}</td>
+      <td>{{post.todo}}</td>
+      <td><button>supprimer</button><button>Modifier</button></td>
+    </tr>
+  </tbody>
+</table>
+     
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'todo',
- 
+  data(){
+    return{
+      posts:null,
+      i:0
+    }
+        
+  },
+ methods:{
+  //async ajouter(){
+           // Création d'une promesse qui va contenir l'appel
+    //       const response = await axios.get('http://localhost:3000/posts')
+            // Avec la méthode "then", on extrait les données
+      //    this.todo=response.data
+        //  console.log(response.data)
+  //}
+  async afficher(){
+   await axios.get("http://localhost:3000/posts")
+            .then(result => {
+              this.posts = result.data;
+              })
+              console.table(this.posts)
+    }
+ }
   
 }
 </script>
@@ -41,5 +70,6 @@ export default {
   padding-left: 200px;
   padding-right: 200px;
 }
+
 
 </style>
